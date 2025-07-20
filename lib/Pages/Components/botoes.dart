a3351140ebbaefcf071a9321381bd6376f8a6ccd
John Victor <johnvictor.trabalho@gmail.com>
2025-07-20 03:16:00 -0300

    Ajustado modelo de botão utilizado no aplicativo

--- lib/Pages/Components/botoes.dart
+++ lib/Pages/Components/botoes.dart
@@ -3,23 +3,29 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final Function()? onTap;
   final String text;
-  const MyButton({super.key, required this.onTap, required this.text, required IconData icon});
+  const MyButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+    required IconData icon,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width:200,
-        height: 70,   
+        width: 200,
+        height: 70,
         padding: const EdgeInsets.all(25),
         decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(8)
-        ),
-        child: Center(child: Text(text, 
-        style: const TextStyle(color: Colors.white,
-        fontWeight: FontWeight.bold),)),
+            color: Colors.black, borderRadius: BorderRadius.circular(8)),
+        child: Center(
+            child: Text(
+          text,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        )),
       ),
     );
   }
