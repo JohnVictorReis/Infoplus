7efea8c33372741ab14bcd5817f51a1ac0400694
John Victor <johnvictor.trabalho@gmail.com>
2025-07-20 03:13:57 -0300

    Authpage Funcional

--- lib/Pages/geral/auth_page.dart
+++ lib/Pages/geral/auth_page.dart
@@ -1,38 +1,3 @@
-// ignore_for_file: unused_import
-
-/*
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:projeto_infoplus/Pages/pagina_inicial.dart';
-import 'package:projeto_infoplus/Pages/pagina_login.dart';
-
-
-class PaginaAuth extends StatelessWidget {
-  const PaginaAuth({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot){
-
-          //Snapshot vai saber se o usuário está logado ou não.
-          if(snapshot.hasData){
-            return const PaginaInicial();
-          }
-
-          else{
-            return const PaginaLogin();
-          }
-        },
-        ),
-    );
-  }
-}
-
- */
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
